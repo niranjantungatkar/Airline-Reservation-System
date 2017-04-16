2f9c87962f6269b89503997e926a880a52c2e783
Niranjan Tungatkar <niranjananil.tungatkar@sjsu.edu>
2017-04-16 13:56:53 -0700

    Initial Commit - Structure Defined, JPA and MySQL dependencies added

---
+++ src/main/java/edu/sjsu/AirlineReservationSystemApplication.java
@@ -0,0 +1,12 @@
+package edu.sjsu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirlineReservationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirlineReservationSystemApplication.class, args);
+	}
+}
