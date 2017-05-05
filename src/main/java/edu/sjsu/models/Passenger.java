f4ce0131511f090290e409930bcab48c9b15b07c
Ajay <ajaytanpure92@gmail.com>
2017-05-04 22:17:20 -0700

    Generator for autogenerating primary key for Passenger

--- src/main/java/edu/sjsu/models/Passenger.java
+++ src/main/java/edu/sjsu/models/Passenger.java
@@ -1,99 +1,93 @@
 package edu.sjsu.models;
 
-import java.util.List;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
 
-import org.hibernate.annotations.Cascade;
-import org.hibernate.annotations.CascadeType;
 import org.hibernate.annotations.GenericGenerator;
 
 @Entity
-@Table(name = "Passenger", uniqueConstraints={@UniqueConstraint(columnNames = {"phone"})})
+@Table(name = "Passenger", uniqueConstraints = { @UniqueConstraint(columnNames = { "phone" }) })
 public class Passenger {
 
 	@Id
-	@GeneratedValue(generator = "uuid")
-	@GenericGenerator(name = "uuid", strategy = "uuid2")
-	private String id;   
-    private String firstname;
-    private String lastname;
-    private int age;
-    private String gender;
-    private String phone;
-  
-    public Passenger() {
-    	super();
-    }
-    
-    public Passenger(String id, String firstname, String lastname, int age, String gender, String phone) {
-    	this.id = id;
-    	this.firstname = firstname;
-    	this.lastname = lastname;
-    	this.age = age;
-    	this.gender = gender;
-    	this.phone = phone;
-    }
-    
-    
+	@GeneratedValue(generator = "passengerNoGenerator")
+	@GenericGenerator(name = "passengerNoGenerator", strategy = "edu.sjsu.utils.PassengerIdGenerator")
+	private String id;
+
+	private String firstname;
+	private String lastname;
+	private int age;
+	private String gender;
+	private String phone;
+
+	public Passenger() {
+		super();
+	}
+
+	public Passenger(String id, String firstname, String lastname, int age, String gender, String phone) {
+		this.id = id;
+		this.firstname = firstname;
+		this.lastname = lastname;
+		this.age = age;
+		this.gender = gender;
+		this.phone = phone;
+	}
+
 	public String getId() {
 		return id;
 	}
-	
+
 	public void setId(String id) {
 		this.id = id;
 	}
-	
+
 	public String getFirstname() {
 		return firstname;
 	}
-	
+
 	public void setFirstname(String firstname) {
 		this.firstname = firstname;
 	}
-	
+
 	public String getLastname() {
 		return lastname;
 	}
-	
+
 	public void setLastname(String lastname) {
 		this.lastname = lastname;
 	}
-	
+
 	public int getAge() {
 		return age;
 	}
-	
+
 	public void setAge(int age) {
 		this.age = age;
 	}
-	
+
 	public String getGender() {
 		return gender;
 	}
-	
+
 	public void setGender(String gender) {
 		this.gender = gender;
 	}
-	
+
 	public String getPhone() {
 		return phone;
 	}
-	
+
 	public void setPhone(String phone) {
 		this.phone = phone;
 	}
 
-	/*public List<Reservation> getReservations() {
-		return reservations;
-	}
-
-	public void setReservations(List<Reservation> reservations) {
-		this.reservations = reservations;
-	}*/
+	/*
+	 * public List<Reservation> getReservations() { return reservations; }
+	 * 
+	 * public void setReservations(List<Reservation> reservations) {
+	 * this.reservations = reservations; }
+	 */
 }
--- src/main/java/edu/sjsu/utils/PassengerIdGenerator.java
+++ src/main/java/edu/sjsu/utils/PassengerIdGenerator.java
@@ -12,27 +12,24 @@ import org.hibernate.id.IdentifierGenerator;
 import com.mysql.jdbc.Connection;
 import com.mysql.jdbc.Statement;
 
-public class ReservationIdGenerator implements IdentifierGenerator {
+public class PassengerIdGenerator implements IdentifierGenerator {
 
 	@Override
 	public Serializable generate(SessionImplementor session, Object object) throws HibernateException {
-
 		Random rand = new Random();
 		char c1 = (char) (rand.nextInt(26) + 'A');
 		char c2 = (char) (rand.nextInt(26) + 'A');
-		String prefix = "RAT";
+		String prefix = "PNT";
 		Connection connection = (Connection) session.connection();
 
 		try {
 			Statement statement = (Statement) connection.createStatement();
 
-			ResultSet rs = statement.executeQuery("select count(orderNumber) as total from Reservation");
+			ResultSet rs = statement.executeQuery("select count(id) as total from Passenger");
 
 			if (rs.next()) {
 				int id = rs.getInt(1) + 100;
 				String generatedId = prefix + new Integer(id).toString() + c1 + c2;
-				System.out.println("-------------------------------------------");
-				System.out.println("Generated Id: " + generatedId);
 				return generatedId;
 			}
 		} catch (SQLException e) {
